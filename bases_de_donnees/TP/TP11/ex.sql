f22a64cf3363b6dfe5e08f1abe3a8b5b55e2c116
mbarek-hani <mbarekhani30@gmail.com>
2025-04-26 15:37:17 +0100

    new content

---
+++ bases_de_donnees/TP/TP11/ex.sql
@@ -0,0 +1,31 @@
+-- Representation(numRep, titreRep, lieuRep)
+-- Musicien(numMus, nom, #numRep)
+-- Programmer(date, tarif, #numRep)
+
+select titreRep as titre from Representation;
+
+select titreRep as titre from Representation where lieu = "théâtre allissa";
+
+select m.nom, r.titreRep as titre
+from Musiscien m
+inner join
+Representation r on
+m.numRep = r.numRep;
+
+select r.titreRep as titre, r.lieuRep as lieu, p.tarif
+from Representation r
+inner join
+Programmer p
+on r.numRep = p.numRep
+where p.date = "2008-07-25";
+
+select count(*) from Representation r 
+inner join 
+Musicien m 
+on r.numRep = m.numRep 
+where r.numRep = 'n°20';
+
+select r.numRep, r.titreRep, r.lieu, p.date from Representation r 
+inner join Programmer p 
+on r.numRep = p.numRep 
+where p.tarif <= 20;
