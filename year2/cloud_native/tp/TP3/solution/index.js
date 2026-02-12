53207ffa077ddd61e3388a01618d2ed77b37febf
mbarek-hani <mbarekhani30@gmail.com>
2026-02-12 16:34:35 +0100

    new content

--- year2/cloud_native/tp/TP3/solution/index.js
+++ year2/cloud_native/tp/TP3/solution/index.js
@@ -1,14 +1,15 @@
-import '@dotenvx/dotenvx/config';
-import express from 'express';
-import livreRouter from './routes/livreRoutes.js';
-import './config/db.js';
+import express from "express";
+import livreRouter from "./routes/livreRoutes.js";
+import "./config/db.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/livres', livreRouter);
+app.use("/livres", livreRouter);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server is running on http://localhost:${PORT}`),
+);
